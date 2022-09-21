ee0171782dd1ffc9eaab53321836dc909d9a408b
ismailarilik <arilik.ismail@gmail.com>
2022-09-21 20:04:42 +0300

    Add PriceOffer component skeleton

---
+++ src/components/PriceOffer.vue
@@ -0,0 +1,3 @@
+<template>
+  Offer
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WeddingHalls from '../components/WeddingHalls.vue'
+import PriceOffer from '../components/PriceOffer.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: WeddingHalls
+    },
+    {
+      path: '/offer',
+      name: 'offer',
+      component: PriceOffer
     }
   ]
 })

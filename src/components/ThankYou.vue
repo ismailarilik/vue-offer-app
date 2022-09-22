390150b62217075851ede7a2cfbff02df34a2b16
ismailarilik <arilik.ismail@gmail.com>
2022-09-22 18:33:05 +0300

    Finish initial design of thank-you page

--- src/components/ThankYou.vue
+++ src/components/ThankYou.vue
@@ -1,3 +1,13 @@
 <template>
-  Thank you!
+  <div class="flex flex-col gap-2 h-screen justify-center items-center">
+    <div class="text-5xl text-white bg-teal-500 text-center w-12 h-12 rounded-full">
+      &#10003;
+    </div>
+    <div class="font-semibold text-teal-500">
+      Fiyat teklifi isteğin başarıyla iletildi.
+    </div>
+    <div>
+      Lazzoni Hotel sana en kısa sürede geri dönüş sağlayacak.
+    </div>
+  </div>
 </template>
